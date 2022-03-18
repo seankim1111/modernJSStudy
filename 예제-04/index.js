a48f0434a4ef238e1bfd362d99619aef830ec767
seankim1111 <yongsun199@gmail.com>
2022-03-18 23:22:33 +0900

    예제풀이04

---
+++ 예제-04/index.js
@@ -0,0 +1,22 @@
+var userId = 1;
+var userName = "Lee";
+
+var user = { id: 1, name: "Lee" };
+
+var users = [
+  { id: 1, name: "Lee" },
+  { id: 2, name: "Kim" },
+];
+
+var result = 10 + 20;
+
+var score;
+
+console.log(score);
+
+var score;
+score = 80;
+
+var score = 80;
+
+console.log(score);
