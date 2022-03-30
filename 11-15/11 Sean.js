a751197ffc1c4df4cce3275ed53b0659f0e7769b
seankim1111 <yongsun199@gmail.com>
2022-03-30 16:22:16 +0900

    11장 예제풀이

--- 11-15/11 Sean.js
+++ 11-15/11 Sean.js
@@ -21,7 +21,114 @@ var str2 = "Hello"; // 5개의 문자로 이뤄진 문자열
 var str = "Hello";
 str = "world";
 
+// 11-04
+var str = "string";
+
+console.log(str[0]); // s
+
+console.log(str.length); // 6
+console.log(str.toUpperCase); // STRING
+
+// 11-05
+var str = "string";
+str[0] = "S";
+
+console.log(str); // string
+
 // 11.1.3 값에 의한 전달
+// 11-06
+var score = 80;
+var copy = score;
+
+console.log(score); // 80
+console.log(copy); // 80
+
+score = 100;
+
+console.log(score); // 100
+console.log(copy); // ?
+
+// 11-07
+var score = 80;
+var copy = score;
+
+console.log(score, copy); // 80, 80
+console.log(score === copy); // true
+
+// 11-08
+score = 100;
+
+console.log(score, copy); // 100, 80
+console.log(score === copy); // false
+
 // 11.2 객체
 // 11.2.1 변경 가능한 값
+// 11-11
+var person = {
+  name: "Lee",
+};
+
+// 11-12
+console.log(person); // {name: "Lee"}
+
+// 11-13
+var person = {
+  name: "Lee",
+};
+
+person.name = "Kim";
+
+person.address = "Seoul";
+
+console.log(person); // {name: 'Kim', address: 'Seoul'}
+
+// 11-14
+const o = { x: { y: 1 } };
+
+const c1 = { ...o };
+console.log(c1 === o); // false
+console.log(c1.x === o.x); // true
+
+const _ = require("lodash");
+
+const c2 = _.cloneDeep(o);
+console.log(c2 === o); // false
+console.log(c2.x === o.x); // false
+
+// 11-15
+const v = 1;
+
+const c1 = v;
+console.log(c1 === v); // true
+
+const o = { x: 1 };
+
+const c2 = o;
+console.log(c2 === o); // true
+
 // 11.2.2 참조에 의한 전달
+
+// 11-16
+var person = {
+  name: "Lee",
+};
+
+var copy = person;
+
+// 11-17
+var person = {
+  name: "Lee",
+};
+
+var copy = person;
+
+console.log(copy === person); // true
+// copy를 통해 객체 변경
+copy.name = "Kim";
+// person을 통해 객체 변경
+person.address = "Seoul";
+
+// 따라서
+
+console.log(person); // {name: 'Kim', address: 'Seoul'}
+console.log(copy); // {name: 'Kim', address: 'Seoul'}
