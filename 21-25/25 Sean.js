51ae6fd8463be0408ea807e7c705d15e30706ed7
seankim1111 <yongsun199@gmail.com>
2022-04-04 17:22:27 +0900

    25장 예제풀이

--- 21-25/25 Sean.js
+++ 21-25/25 Sean.js
@@ -23,11 +23,63 @@ var me = new Person("Lee");
 me.sayHi(); // Hi! My name is Lee
 
 // 25.2 클래스 정의
-25 - 02;
+// 25-02
 // 클래스 선언문
 class Person {}
 
+// 익명 클래스 표현식
+const Person = class {};
+
+// 기명 클래스 표현식
+const Person = class MyClass {};
+
+// 25-04
+// 클래스 선언문
+class Person {
+  // 생성자
+  constructor(name) {
+    // 인스턴스 생성 및 초기화
+    this.name = name; // name 프로퍼티는 public 하다.
+  }
+
+  // 프로토타입 메서드
+  sayHi() {
+    console.log(`Hi! My name is ${this.name}`);
+  }
+
+  // 정적 메서드
+  static sayHello() {
+    console.log("Hello!");
+  }
+}
+
+// 인스턴스 생성
+const me = new Person("Lee");
+
+// 인스턴스의 프로퍼티 참조
+console.log(me.name); // Lee
+// 프로토타입 메서드 호출
+me.sayHi(); // Hi! My name is Lee
+// 정적 메서드 호출
+Person.sayHello(); // Hello!
+
 // 25.3 클래스 호이스팅
+// 클래스 선언문
+class Person {}
+
+console.log(typeof Person); // function
+
+const Person = "";
+
+{
+  // 호이스팅이 발생하지 않는다면 ''이 출력되어야 한다.
+  console.log(Person);
+  // ReferenceError:
+
+  // 클래스 선언문
+  class Person {}
+}
+
 // 25.4 인스턴스 생성
 // 25.5 메서드
 // ____25.5.1 constructor
