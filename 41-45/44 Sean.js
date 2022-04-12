e6cc878f655807c69a3840bc5f88ad186530b5fa
seankim1111 <yongsun199@gmail.com>
2022-04-12 23:12:20 +0900

    44장 예제풀이

---
+++ 41-45/44 Sean.js
@@ -0,0 +1,12 @@
+// 44장 REST API
+// 44.1 REST API의 구성
+// 44.2 REST API 설계 원칙
+// 44.3 JSON Server를 이용한 REST API 실습
+// ____44.3.1 JSON Server 설치
+// ____44.3.2 db.json 파일 생성
+// ____44.3.3 JSON Server 실행
+// ____44.3.4 GET 요청
+// ____44.3.5 POST 요청
+// ____44.3.6 PUT 요청
+// ____44.3.7 PATCH 요청
+// ____44.3.8 DELETE 요청
