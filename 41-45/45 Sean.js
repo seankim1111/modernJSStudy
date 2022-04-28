0e252d1c7576e2dd5790cee1c07b156486bd414e
seankim1111 <yongsun199@gmail.com>
2022-04-28 19:07:43 +0900

    45장 예제풀이 진행중

--- 41-45/45 Sean.js
+++ 41-45/45 Sean.js
@@ -172,15 +172,15 @@ try {
 
 // 45.2 프로미스의 생성
 // 45-10
-// 프로미스 생성
-const promise = new Promise ((resolve, reject) => {
-  // Promise 함수의 콜백 함수 내부에서 비동기 처리를 수행한다.
-  if(/* 비동기 처리 성공 */) {
-    resolve('result');
-  } else{ /* 비동기 처리 실패 */  
-reject('failure reason');
-  }
-});
+// // 프로미스 생성
+// const promise = new Promise ((resolve, reject) => {
+//   // Promise 함수의 콜백 함수 내부에서 비동기 처리를 수행한다.
+//   if(/* 비동기 처리 성공 */) {
+//     resolve('result');
+//   } else{ /* 비동기 처리 실패 */
+// reject('failure reason');
+//   }
+// });
 
 // 45-11
 
