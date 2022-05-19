a7301ece1459a8bc8d51b33e7693f5643beb95b6
김선주 <sseunn123@naver.com>
2022-05-19 20:09:05 +0900

    33강완료

--- 31-35(4.05)/sseunn33.js
+++ 31-35(4.05)/sseunn33.js
@@ -1,57 +1,156 @@
-//[예제풀이 13 -01]
-//[예제풀이 13 -02]
-//[예제풀이 13 -03]
-//[예제풀이 13 -04]
-//[예제풀이 13 -05]
-//[예제풀이 13 -06]
-//[예제풀이 13 -07]
-//[예제풀이 13 -08]
-//[예제풀이 13 -09]
-//[예제풀이 13 -10]
-//[예제풀이 13 -11]
-//[예제풀이 13 -13]
-//[예제풀이 13 -13]
-//[예제풀이 13 -14]
-//[예제풀이 13 -15]
-//[예제풀이 13 -16]
-//[예제풀이 13 -17]
-//[예제풀이 13 -18]
-//[예제풀이 13 -19]
-//[예제풀이 13 -20]
-//[예제풀이 13 -21]
-//[예제풀이 13 -22]
-//[예제풀이 13 -23]
-//[예제풀이 13 -24]
-//[예제풀이 13 -25]
-//[예제풀이 13 -26]
-//[예제풀이 13 -27]
-//[예제풀이 13 -28]
-//[예제풀이 13 -29]
-//[예제풀이 13 -30]
-//[예제풀이 13 -31]
-//[예제풀이 13 -32]
-//[예제풀이 13 -33]
-//[예제풀이 13 -34]
-//[예제풀이 13 -35]
-//[예제풀이 13 -36]
-//[예제풀이 13 -37]
-//[예제풀이 13 -38]
-//[예제풀이 13 -39]
-//[예제풀이 13 -40]
-//[예제풀이 13 -41]
-//[예제풀이 13 -42]
-//[예제풀이 13 -43]
-//[예제풀이 13 -44]
-//[예제풀이 13 -45]
-//[예제풀이 13 -46]
-//[예제풀이 13 -47]
-//[예제풀이 13 -48]
-//[예제풀이 13 -49]
-//[예제풀이 13 -50]
-//[예제풀이 13 -51]
-//[예제풀이 13 -52]
-//[예제풀이 13 -53]
-//[예제풀이 13 -54]
-//[예제풀이 13 -55]
-//[예제풀이 13 -56]
-//[예제풀이 13 -57]
+//[예제풀이 33 -01]
+//Symbol 함수를 호출하여 유일무이한 심벌 값을 생성한다.
+const mySymbol = Symbol();
+console.log(typeof mySymbol); //symbol
+//심벌 값은 외부로 노출되지 않아 확인할 수 없다.
+console.log(mySymbol); //Symbol()
+
+//[예제풀이 33-02]
+new Symbol(); //TypeError: Symbol is not a constructor
+
+//[예제풀이 33-03]
+//심벌 값에 대한 설명이 같더라도 유일무이한 심벌 값을 생성한다.
+const mySymbol1 = Symbol("mySymbol");
+const mySymbol2 = Symbol("mySymbol");
+console.log(mySymbol1 === mySymbol2); //false
+
+//[예제풀이 33-04]
+const mySymbol = Symbol("mySymbol");
+//심벌도 레퍼 객체를 생성한다
+console.log(mySymbol.description); //mySymbol
+console.log(mySymbol.toString()); //Symbol(mySymbol)
+
+//[예제풀이 33-05]
+const mySymbol = Symbol();
+// 심벌 값은 암묵적으로 문자열이나 숫자 타입으로 변환되지 않는다.
+console.log(mySymbol + ""); //TypeError: Cannot convert a Symbol value to a string
+console.log(+mySymbol); //TypeError: Cannot convert a Symbol value to a string
+
+//[예제풀이 33-06]
+const mySymbol = Symbol();
+//불리언 타입으로는 암묵적으로 타입 변환된다.
+console.log(!!mySymbol); // true
+//if 문 등에서 존재 확인을 위해 사용할 수 있다.
+if (mySymbol) console.log("mySymbol is not empty.");
+
+//[예제풀이 33-07]
+// 전역 심벌 레지스트리에 mySymbol이라는 키로 저장된 심벌 값이 없으면 새로운 심벌 값을 생성
+const s1 = Symbol.for("mySymbol");
+// 전역 심벌 레지스트리에 mySymbol이라는 키로 저장된 심벌 값이 있으면 해당 심벌 값을 반환
+const s2 = Symbol.for("mySymbol");
+console.log(s1 === s2); // true
+
+//[예제풀이 33-08]
+// 전역 심벌 레지스트리에 mySymbol이라는 키로 저장된 심벌 값이 없으면 새로운 심벌 값을 생성
+const s1 = Symbol.for("mySymbol");
+// 전역 심벌 레지스트리에 저장된 심벌 값의 키를 추출
+Symbol.keyFor(s1); // -> mySymbol
+// Symbol 함수를 호출하여 생성한 심벌 값은 전역 심벌 레지스트리에 등록되어 관리되지 않는다.
+const s2 = Symbol("foo");
+// 전역 심벌 레지스트리에 저장된 심벌 값의 키를 추출
+Symbol.keyFor(s2); // -> undefined
+
+//[예제풀이 33-09]
+// 위, 아래, 왼쪽, 오른쪽을 나타내는 상수를 정의한다.
+// 이때 값 1, 2, 3, 4에는 특별한 의미가 없고 상수 이름에 의미가 있다.
+const Direction = {
+  UP: 1,
+  DOWN: 2,
+  LEFT: 3,
+  RIGHT: 4,
+};
+// 변수에 상수를 할당
+const myDirection = Direction.UP;
+if (myDirection === Direction.UP) {
+  console.log("You are going UP.");
+}
+
+//[예제풀이 33-10]
+// 위, 아래, 왼쪽, 오른쪽을 나타내는 상수를 정의한다.
+// 중복될 가능성이 없는 심벌 값으로 상수 값을 생성한다.
+const Direction = {
+  UP: Symbol("up"),
+  DOWN: Symbol("down"),
+  LEFT: Symbol("left"),
+  RIGHT: Symbol("right"),
+};
+const myDirection = Direction.UP;
+if (myDirection === Direction.UP) {
+  console.log("You are going UP.");
+}
+
+//[예제풀이 33-11]
+// JavaScript enum
+// Direction 객체는 불변 객체이며 프로퍼티는 유일무이한 값이다.
+const Direction = Object.freeze({
+  UP: Symbol("up"),
+  DOWN: Symbol("down"),
+  LEFT: Symbol("left"),
+  RIGHT: Symbol("right"),
+});
+const myDirection = Direction.UP;
+if (myDirection === Direction.UP) {
+  console.log("You are going UP.");
+}
+
+//[예제풀이 33-12]
+const obj = {
+  // 심벌 값으로 프로퍼티 키를 생성
+  [Symbol.for("mySymbol")]: 1,
+};
+obj[Symbol.for("mySymbol")]; //1
+
+//[예제풀이 33-13]
+const obj = {
+  // 심벌 값으로 프로퍼티 키를 생성
+  [Symbol("mySymbol")]: 1,
+};
+for (const key in obj) {
+  console.log(key); // 아무것도 출력되지 않는다.
+}
+console.log(Object.keys(obj)); // []
+console.log(Object.getOwnPropertyNames(obj)); // []
+
+//[예제풀이 33-14]
+const obj = {
+  // 심벌 값으로 프로퍼티 키를 생성
+  [Symbol("mySymbol")]: 1,
+};
+// getOwnPropertySymbols 메서드는 인수로 전달한 객체의 심벌 프로퍼티 키를 배열로 반환한다.
+console.log(Object.getOwnPropertySymbols(obj)); // [Symbol(mySymbol)]
+//getOwnPropertySymbols 메서드로 심벌 값도 찾을 수 있다.
+const symbolKey1 = Object.getOwnPropertySymbols(obj)[0];
+console.log(obj[symbolKey1]); // 1
+
+//[예제풀이 33-15]
+//표준 빌트인 객체를 확장하는 것은 권장하지 않는다.
+Array.prototype.sum = function () {
+  return this.reduce((acc, cur) => acc + cur, 0);
+};
+[1, 2].sum(); //3
+
+//[예제풀이 33-16]
+//심벌 값으로 프로퍼티 키를 동적 생성하면 다른 프로퍼티 키와 절대 충돌하지 않아 안전하다.
+Array.prototype[Symbol.for("sum")] = function () {
+  return this.reduce((acc, cur) => acc + cur, 0);
+};
+[1, 2][Symbol.for("sum")](); //3
+
+//[예제풀이 33-17]
+//1~5 범위의 정수로 이루어진 이터러블
+const iterable = {
+  // ymbol.iterator 메서드를 구현하여 이터러블 프로토콜을 준수
+  [Symbol.iterator]() {
+    let cur = 1;
+    const max = 5;
+    //Symbol.iterator 메서드는 next 메서드를 소유한 이터레이터를 반환
+    return {
+      next() {
+        return { value: cur++, done: cur > max + 1 };
+      },
+    };
+  },
+};
+for (const num of iterable) {
+  console.log(num); //1 2 3 4 5
+}
